5b020c614179f387fd4bf763c50172ea9a322bd8
Lugas Septiawan <lugassawan@gmail.com>
2018-11-12 14:50:31 +0700

    add email data

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.get(`/api/emails`, (req, res) => {
+	const emails = [
+		{ id: 1, email: "jhon@gmail.com" },
+		{ id: 2, email: "will@gmail.com" },
+		{ id: 3, email: "doejhon@gmail.com" }
+	];
+
+	res.json(emails);
+});
+
 app.get(`/api/customers`, (req, res) => {
 	const customers = [
 		{ id: 1, firstName: "Lugas", lastName: "Doe" },
